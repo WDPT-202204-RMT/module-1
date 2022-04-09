f413e1e6326e8432bc22bb2bc8a2884653bba5e7
Enes-Ironhack <enes1.koc@epitech.eu>
2022-04-09 12:38:20 +0200

    lot of informations about functions

--- week1/day3/index.js
+++ week1/day3/index.js
@@ -63,4 +63,75 @@ const printEveryChar4 = (str) => {
 // printEveryChar('Enes');
 // printEveryChar2('Test');
 //printEveryChar3('three');
-printEveryChar4('four');
+//printEveryChar4('four');
+
+// print the result directly
+function add(nb1, nb2) {
+  console.log(nb1 + nb2);
+  return; // this is good practice
+}
+
+function addWithReturn(nb1, nb2) {
+  return nb1 + nb2;
+}
+
+add(5, 5);
+let var2 = addWithReturn(5, 5); // this is equal to 10
+console.log(var2);
+
+let result = addWithReturn(2, 5);
+console.log(result);
+
+// let result = add(5, 5);
+// console.log(result);
+
+// chain functions together.
+console.log(addWithReturn(5, 5) + addWithReturn(2, 2));
+
+const test = function () {
+  console.log('hello');
+  return; // this stops the function.
+  // break; this only works in loops
+  console.log('this is a test');
+};
+
+test();
+
+// function checkAge(age) {
+//   if (age > 18) {
+//     return 'you are an adult';
+//   } else {
+//     return 'you are a child';
+//   }
+// }
+
+// function checkAge(age) {
+//   if (age > 18) return 'you are an adult';
+//   else return 'you are a child';
+// }
+
+//with ternary operators
+function checkAge(age) {
+  // if true      do this             else do this
+  return age > 18 ? 'you are an adult' : 'you are an adult';
+}
+
+console.log(checkAge(19));
+
+// function that returns more than one thing
+// don't focus on this for now
+
+function getUserInfo(firstName, lastName) {
+  //console.log(firstName + ' ' + lastName);
+  //object
+  let userInfo = {
+    firstName: firstName,
+    lastName: lastName,
+    age: 24,
+    city: 'Bodeaux',
+  };
+
+  return userInfo;
+}
+
+console.log(getUserInfo('Enes', 'Koc'));
